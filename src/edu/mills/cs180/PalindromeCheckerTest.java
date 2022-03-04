b315c93c1b841fa0172826c2dbd6d4c45131dd4c
jyp17 <jypark1024@gmail.com>
2022-03-04 14:19:21 -0800

    Add test class

---
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -0,0 +1,14 @@
+package edu.mills.cs180;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.*;
+
+class PalindromeCheckerTest {
+
+    @Test
+    void isPalindrome_True_EmptyString() {
+        String s = "";
+        assertTrue(PalindromeChecker.isPalindrome(s));
+    }
+
+}
